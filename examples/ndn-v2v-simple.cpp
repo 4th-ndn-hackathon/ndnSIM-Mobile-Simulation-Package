8de075fb57cf16358e6c408fac166c3c33f3664c
Spyridon Mastorakis <spiros.mastorakis@gmail.com>
2017-03-26 09:43:06 -0500

    Documentation added

--- examples/ndn-v2v-simple.cpp
+++ examples/ndn-v2v-simple.cpp
@@ -17,18 +17,39 @@
 
 namespace ns3{
 
-NS_LOG_COMPONENT_DEFINE ("V2VTest");
+/**
+ * This scenario simulates a scenario with 6 cars movind and communicating
+ * in an ad-hoc way.
+ *
+ * 5 consumers request data from producer with frequency 1 interest per second
+ * (interests contain constantly increasing sequence number).
+ *
+ * For every received interest, producer replies with a data packet, containing
+ * 1024 bytes of payload.
+ *
+ * To run scenario and see what is happening, use the following command:
+ *
+ *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=ndn-v2v-simple
+ *
+ * To modify the mobility model, see function installMobility.
+ * To modify the wifi model, see function installWifi.
+ * To modify the NDN settings, see function installNDN and for consumer and
+ * producer settings, see functions installConsumer and installProducer
+ * respectively.
+ */
+
+NS_LOG_COMPONENT_DEFINE ("V2VSimple");
 
 
 static const uint32_t numNodes = 6;
-  
+
 void printPosition(Ptr<const MobilityModel> mobility) //DEBUG purpose
 {
   Simulator::Schedule(Seconds(1), &printPosition, mobility);
   NS_LOG_INFO("Car "<<  mobility->GetObject<Node>()->GetId() << " is at: " <<mobility->GetPosition());
 }
 
-  
+
 void installMobility(NodeContainer &c, int simulationEnd)
 {
   MobilityHelper mobility;
@@ -42,13 +63,13 @@ void installMobility(NodeContainer &c, int simulationEnd)
       wayMobility[i] = c.Get(i)->GetObject<WaypointMobilityModel>();
       Waypoint waypointStart(Seconds(0), Vector3D(i*10, 0, 0));
       Waypoint waypointEnd(Seconds(simulationEnd), Vector3D(i*10, 0, 0));
-      
+
       wayMobility[i]->AddWaypoint(waypointStart);
       wayMobility[i]->AddWaypoint(waypointEnd);
       NS_LOG_INFO("Node " << i << " positions " << waypointStart << " " << waypointEnd);
     }
 
-    
+
     return;
   }
   Ptr<WaypointMobilityModel> wayMobility[numNodes];
@@ -63,9 +84,9 @@ void installMobility(NodeContainer &c, int simulationEnd)
     wayMobility[i]->AddWaypoint(waypointEnd);
     NS_LOG_INFO("Node " << i << " positions " << waypointStart << " " << waypointMiddle << " " << waypointEnd);
   }
-  
 
-  
+
+
 }
 
 
@@ -73,16 +94,16 @@ void installWifi(NodeContainer &c, NetDeviceContainer &devices)
 {
   // Modulation and wifi channel bit rate
   std::string phyMode("OfdmRate24Mbps");
-  
+
   // Fix non-unicast data rate to be the same as that of unicast
   Config::SetDefault("ns3::WifiRemoteStationManager::NonUnicastMode", StringValue(phyMode));
-  
+
   WifiHelper wifi;
   wifi.SetStandard(WIFI_PHY_STANDARD_80211a);
-  
+
   YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default();
   wifiPhy.SetPcapDataLinkType(YansWifiPhyHelper::DLT_IEEE802_11_RADIO);
-  
+
   YansWifiChannelHelper wifiChannel;
   wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
   wifiChannel.AddPropagationLoss("ns3::RangePropagationLossModel",
@@ -92,17 +113,17 @@ void installWifi(NodeContainer &c, NetDeviceContainer &devices)
                                  "m1", DoubleValue(1.0),
                                  "m2", DoubleValue(1.0));
   wifiPhy.SetChannel(wifiChannel.Create());
-  
+
   // Add a non-QoS upper mac
   NqosWifiMacHelper wifiMac = NqosWifiMacHelper::Default();
   // Set it to adhoc mode
   wifiMac.SetType("ns3::AdhocWifiMac");
-  
+
   // Disable rate control
   wifi.SetRemoteStationManager("ns3::ConstantRateWifiManager",
                                "DataMode", StringValue(phyMode),
                                "ControlMode", StringValue(phyMode));
-  
+
   devices = wifi.Install(wifiPhy, wifiMac, c);
 }
 
@@ -115,7 +136,7 @@ void installNDN(NodeContainer &c)
   ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/broadcast");
 
   ///todo add v2v face
-  
+
 
 }
 
@@ -142,18 +163,18 @@ void installProducer(NodeContainer &c)
 int main (int argc, char *argv[])
 {
   NS_LOG_UNCOND ("V2VTest Simulator");
-  
+
   uint32_t numProducer = 1;
   int simulationEnd = 10;
-  
+
   NodeContainer c;
   c.Create(numNodes);
-  
+
   installMobility(c, simulationEnd);
-  
+
   NetDeviceContainer netDevices;
   installWifi(c, netDevices);
-  
+
   installNDN(c);
 
   //setting application
@@ -162,7 +183,7 @@ int main (int argc, char *argv[])
 
   NodeContainer producer;
   producer.Add(c.Get(producerId));
-  
+
   NodeContainer consumers;
   for(int i=0; i<numNodes; i++){
     if(i!=producerId){
@@ -170,16 +191,16 @@ int main (int argc, char *argv[])
       break;//tmp
     }
   }
-  
+
   installConsumer(consumers);
   installProducer(producer);
-  
-  
-  
+
+
+
   for(int i=0; i<c.GetN(); i++){
     Simulator::Schedule(Seconds(1), &printPosition, c.Get(i)->GetObject<WaypointMobilityModel>());
   }
-  
+
   Simulator::Stop(Seconds(simulationEnd));
 
   std::string animFile = "v2v-test.xml";
